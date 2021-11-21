aded2580fcc0b3f58e58eb066a47a3184b18393f
mairid <mairiduubik@gmail.com>
2021-11-21 21:46:01 +0200

    Advanced visual

--- src/components/Contents.vue
+++ src/components/Contents.vue
@@ -1,9 +1,15 @@
 <template>
   <div id="contents">
     <p class="item" v-for="post in postList" :key="post.id">
-      <span class="author"> {{ post.author }} </span> <br />
-      <span class="image"> <img :src="post.image" /> </span> <br />
-      <span class="subtext"> {{ post.subtext }} </span>
+        <span class="postHeader">
+            <span class="author"> {{ post.author }} </span>
+            <span class="time"> {{post.time}} </span> <br />
+        </span>
+        <span class="image"> <img :src="post.image" /> </span> <br />
+        <span class="postFooter">
+            <span class="subtext"> {{ post.subtext }} </span> <br />
+            <span class="like"> {{post.like}} </span>
+        </span>
     </p>
   </div>
 </template>
@@ -25,40 +31,75 @@ export default {
 
 <style scoped>
 .item {
-  background: rgb(128, 183, 235);
+  background: white;
   margin-bottom: 5px;
-  padding: 3px 5px;
-  border-radius: 5px;
+  padding: 3px 0px;
+  border-radius: 0px;
+  box-shadow: 10px 2px 30px rgba(0, 0, 0, 0.2);
 }
 
 #contents {
-  background: #1862c9;
-  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.2);
+  background: #c9c9c9;
+  box-shadow: 1px 2px 30px rgba(0, 0, 0, 0.2);
   margin-bottom: 30px;
-  padding: 10px 20px;
-  border-radius: 10px;
+  padding: 10px 70px;
+  border-radius: 0px;
   width: 50%;
-  margin-left: 24%;
+  margin: auto;
   margin-top: 1%;
   margin-bottom: 50px;
 }
-#contents ul {
-  padding: 0;
-  list-style-type: none;
-}
-#contents li {
-  margin-right: 10px;
-  margin-top: 10px;
-  padding: 20px;
-  background: rgba(255, 255, 255, 0.7);
-}
+
 img {
   /*Image box, so it doesn't overflow*/
-  max-width: 70%;
-  max-height: 70%;
+  max-width: 100%;
   display: inline;
+  float: center;
 }
 .author {
+    float: left;
+    margin-left: 23px;
+    margin-top: 0px;
+    font-size: 14px;
+    
+}
+.time {
+    float: right;
+    font-size: 70%;
+    font-size: 14px;
+    margin-top: 0px;
+
+}
+/*Every post's header:  time and user icon*/
+.postHeader {                         
+    height: 20px;
+    background: url(../assets/usericon.png);
+    background-size: 20px 20px;
+    background-repeat: no-repeat;
+    background-position: left;
+    margin: 2px;
+    color: gray;
+    display: block;
+    font-size: 14px;
+    font-weight: bold;
+    padding: 5px;
+}
+.postFooter{
+    height: inherit;
+    margin: 2px;
+    background-color: white;
+    text-decoration-color: black;
     text-align: left;
+    display: block;
+    font-size: 14px;
+    font-weight: bold;
+    padding: 5px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+}
+.subtext {
+    font-weight: bold;
+    
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -6,16 +6,16 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     postList:[
-      {id: 1, author: "Charlotte Bronte", image: require('@/assets/batman_robin.jpg'), subtext: "Logo"},
-      {id: 2, author: "Michael Bronte", image: require('@/assets/tornado.jpg'), subtext: "Logo"},
-      {id: 3, author: "Charlotte Bronte", image: require('@/assets/logo.png'), subtext: "Logo"},
-      {id: 4, author: "Charlotte Bronte", image: require('@/assets/logo.png'), subtext: "Logo"},
-      {id: 5, author: "Charlotte Bronte", image: require('@/assets/logo.png'), subtext: "Logo"},
-      {id: 6, author: "Charlotte Bronte", image: require('@/assets/logo.png'), subtext: "Logo"},
-      {id: 7, author: "Charlotte Bronte", image: require('@/assets/logo.png'), subtext: "Logo"},
-      {id: 8, author: "Charlotte Bronte", image: require('@/assets/logo.png'), subtext: "Logo"},
-      {id: 9, author: "Charlotte Bronte", image: require('@/assets/logo.png'), subtext: "Logo"},
-      {id: 10, author: "Charlotte Bronte", image: require('@/assets/logo.png'), subtext: "Logo"},
+      {id: 1, author: "Michael Bronte", time: "Sep 12 2021, 13:15", subtext: "Hello!"},
+      {id: 2, author: "Elle Bernhard", time: "Sep 14 2021, 17:18",image: require('@/assets/tornado.jpg'), subtext: "Looks like it's going to rain"},
+      {id: 3, author: "Matt Smith", time: "Sep 15 2021, 23:34",image: require('@/assets/batman_robin.jpg'), subtext: "Best friends forever"},
+      {id: 4, author: "Michelle Woodbar", time: "Sep 22 2021, 17:59",image: require('@/assets/dinner.png'), subtext: "Best dinner I've had yet :)"},
+      {id: 5, author: "Billie Waterboard", time: "Sep 22 2021, 18:12", subtext: "Logo"},
+      {id: 6, author: "Anthon Callister", time: "Sep 29 2021, 22:21", subtext: "Logo"},
+      {id: 7, author: "William Willing", time: "Sep 30 2021, 11:00",image: require('@/assets/birthday.jpg'), subtext: "Happy birthday to me!"},
+      {id: 8, author: "Brody Broom", time: "Oct 1 2021, 23:45",image: require('@/assets/hogwarts.png'), subtext: "Missing my school :'( :( :("},
+      {id: 9, author: "Billie Waterboard", time: "Oct 14 2021, 17:17", subtext: "Logo"},
+      {id: 10, author: "Michael Bronte", time: "Oct 24 2021, 08:01",image: require('@/assets/forest.jpg'), subtext: "Guess what time it is... :D"},
   ]
   },
   mutations: {},
