190dcfd4b90a538da4f1cf8550278cc9fb86a651
mairid <mairiduubik@gmail.com>
2021-12-11 00:59:12 +0200

    Edited css and added likes from db

---
+++ src/components/Posts.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="posts">
+    <contents> </contents>
+  </div>
+</template>
+
+<script>
+
+import Contents from "@/components/Contents.vue";
+
+export default {
+
+  name: "Posts",
+  components: { Contents },
+  props: ["PostList"],
+  data() {
+    return{
+    }}}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
